972a0e3cdefa6934e8158a71095bcac76c13e568
Mitch Stevens <mitchstevens95@gmail.com>
2025-11-13 15:09:56 +1100

    minecraft server service can be installed with ./install_service.sh

--- install_service.sh
+++ install_service.sh
@@ -1,1 +1,5 @@
-docker build -t minecraft-create-server .
+cp -rp . /opt/minecraft
+cp minecraft.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable minecraft
+systemctl start minecraft
